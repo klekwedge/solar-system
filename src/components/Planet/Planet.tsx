9d1fa67cd4c6164f5d8fd401691f0924906e5f40
Andrey <antonov.andrey.10101@gmail.com>
2023-02-16 09:11:14 +0300

    feat: add real speeds for planets

--- src/components/Planet/Planet.tsx
+++ src/components/Planet/Planet.tsx
@@ -17,13 +17,14 @@ function Planet({ radius, orbitalSpeed, orbitDimensions, speedAroundAxis, planet
       h={`${orbitDimensions}em`}
       mt={`-${orbitDimensions / 2}em`}
       ml={`-${orbitDimensions / 2}em`}
-      // animation={`orbitAnim ${orbitalSpeed}s linear 0s infinite`}
+      animation={`orbitAnim ${orbitalSpeed}s linear 0s infinite`}
     >
       <Image
         className="planet"
-        left="50%"
+        left={`${50}%`}
+        transform={`translateY(-${50}%)`}
         top="0"
-        // animation={`rotateAnim ${speedAroundAxis}s linear 0s infinite`}
+        animation={`rotateAnim ${speedAroundAxis}s linear 0s infinite`}
         src={planetImg}
         w="70px"
         h="70px"
